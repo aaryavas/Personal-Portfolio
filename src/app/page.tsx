ae5de95e167299a701be17b3b70757aa50191491
aaryavas <aaryavie@gmail.com>
2025-05-14 18:13:53 -0400

    Text Animation Feature - Typewriter ||COMPLETE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,54 @@
 'use client'
 import Image from "next/image";
 import { ReactTyped } from "react-typed";
-
-
-
+import { useState, useEffect } from "react";
+import "./globals.css"
 export default function Home() {
+
+  
+  /*build cursor load handler*/
+  const [cursor, cursorStatus] = useState(false);
+  //prior to delay hideCursor, after delay show cursor
+  useEffect(()=> {
+    console.log("Cursor Show Effect Loading");
+    const delay = 3510;
+    const timeout = setTimeout(() =>{
+      //showing cursor
+      cursorStatus(true);
+      console.log("Waiting for Cursor ...");
+    }, delay)
+    // now need to clear the timeout 
+    return () => {
+      clearTimeout(timeout);
+    }
+  },[]); //the array being empty means it only runs once
+  const [hideCursor, setHideCursor] = useState(false);
+  
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* this part is for the image replace to Header Text*/}
       <main className="flex flex-col gap-[12px] row-start-2 items-center sm:items-start">
-        <h1 className= "text-[39px] font-[family-name:var(--font-geist-mono)]">
-          <ReactTyped
-            strings={["Aarya Vasantlal."]}
-            typeSpeed={100}
-            showCursor={true}
-            cursorChar="|"
-          />
-          </h1>       
+        <h1 className="text-[39px] font-[family-name:var(--font-geist-mono)]">
+          <div className={hideCursor ? "hide-cursor" : ""}>
+            <ReactTyped
+              startDelay={500}
+              strings={["Aarya Vasantlal."]}
+              typeSpeed={120}
+              showCursor={true}
+              cursorChar="|"
+              onComplete={() => setHideCursor(true)}
+            />
+          </div>
+        </h1>
         <h2 className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
            {/* ol - ordered list*/}
           <div className="mb-2 tracking-[-.01em]">
-            
             <ReactTyped
-            startDelay={3000}
+            onStart={() => cursorStatus(true)}
+            startDelay={3510}
             strings={["Welcome to my home."]}
-            typeSpeed={80}
-            showCursor={false}
+            typeSpeed={100}
+            showCursor={cursor}
             cursorChar="|"
             />
           </div>
